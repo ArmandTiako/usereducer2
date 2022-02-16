de63a0e95ba3b83ed470bf0e3efc58f31224f667
Armand Tiako <armand_tiako@yahoo.fr>
2022-02-16 17:38:21 +0100

    Initial commit

---
+++ src/components/Count.js
@@ -0,0 +1,83 @@
+import React, { useReducer } from "react";
+
+const initialState = {
+  countOne: 0,
+  countTwo: 0,
+};
+
+const reducer = (state, action) => {
+  console.log(state);
+  console.log(action);
+  switch (action.type) {
+    case "increment":
+      return { ...state, countOne: state.countOne + action.value };
+
+    case "decrement":
+      return { ...state, countOne: state.countOne - action.value };
+
+    case "increment2":
+      return { ...state, countTwo: state.countTwo + action.value };
+
+    case "decrement2":
+      return { ...state, countTwo: state.countTwo - action.value };
+
+    case "reinitialiser":
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+const Count = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="row">
+      <div className="col-6">
+        <p className="h1 mt-3"> {count.countOne} </p>
+        <button
+          className="btn btn-success m-3"
+          onClick={() => dispatch({ type: "increment", value: 1 })}
+        >
+          +
+        </button>
+
+        <button
+          className="btn btn-danger m-3"
+          onClick={() => dispatch({ type: "decrement", value: 1 })}
+        >
+          -
+        </button>
+      </div>
+
+      <div className="col-6">
+        <p className="h1 mt-3"> {count.countTwo} </p>
+        <button
+          className="btn btn-success m-3"
+          onClick={() => dispatch({ type: "increment2", value: 1 })}
+        >
+          +
+        </button>
+
+        <button
+          className="btn btn-danger m-3"
+          onClick={() => dispatch({ type: "decrement2", value: 1 })}
+        >
+          -
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="btn btn-primary m-3"
+          onClick={() => dispatch({ type: "reinitialiser" })}
+        >
+          0
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Count;
